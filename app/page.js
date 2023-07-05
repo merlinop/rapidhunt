dba9ef847f144ed681b4f2238856098ee6721652
OddWays <95982923+merlinop@users.noreply.github.com>
2023-07-05 14:20:22 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Summary from "@/components/Summary"
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen bg-[#312e42] text-white flex items-center justify-center ">
+    <div className="w-full min-h-screen bg-[#312e42] text-white flex items-center justify-center pt-2 ">
       {/* Sub Center */}
       <div className=" w-[85%] lg:w-[65%] pb-4 h-[95vh] mx-auto rounded-2xl  bg-[#201f2e] text-sm overflow-y-scroll scrollbar-hide md:overflow-visible">
         {/* Header */}
