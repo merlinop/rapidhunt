d09dc7a3650d1ace7c5d615ca3e501bc48929a5d
OddWays <95982923+merlinop@users.noreply.github.com>
2023-07-05 14:11:55 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="w-full min-h-screen bg-[#312e42] text-white flex items-center justify-center ">
       {/* Sub Center */}
-      <div className=" w-[85%] lg:w-[65%] pb-4 h-[95vh] mx-auto rounded-2xl  bg-[#201f2e] text-sm overflow-y-scroll md:overflow-visible">
+      <div className=" w-[85%] lg:w-[65%] pb-4 h-[95vh] mx-auto rounded-2xl  bg-[#201f2e] text-sm overflow-y-scroll scrollbar-hide md:overflow-visible">
         {/* Header */}
         <Headers />
 
